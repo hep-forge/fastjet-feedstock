05ae5eb0813bd1ba57c8b73cb6b85983f922ea73
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-07-17 09:57:38 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,8 @@
 
 make -j$(nproc)
 make install
+
+ln -sf libfastjet.so $PREFIX/lib/libfastjet.so.0
+ln -sf libfastjettools.so $PREFIX/lib/libfastjettools.so.0
+ln -sf libfastjetplugins.so $PREFIX/lib/libfastjetplugins.so.0
+ln -sf libfastjetcontribfragile.so $PREFIX/lib/libfastjetcontribfragile.so.0
